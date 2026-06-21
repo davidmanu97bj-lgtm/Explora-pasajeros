80881f650b3feda79fede836f8440339508a4b26
davidmanu97bj-lgtm <davidmanu97bj@gmail.com>
2026-06-21 17:52:54 -0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'explora-pwa-v7-dual-restaurants';
+const CACHE_NAME = 'explora-pwa-v8-canvas-restaurants';
 const APP_SHELL = [
   './', './index.html', './manifest.webmanifest',
   './icons/icon-192.png', './icons/icon-512.png',
